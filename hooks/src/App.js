244308f65aa28458d2bab144f76f524d2ff44c31
Vashim Nadaf <vashimnadaf12@gmail.com>
2025-04-19 14:22:38 +0530

    Destructuring

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,14 +1,17 @@
 // import './style.css';
 // import UseState from './UseState'
 
-import Time from './Challenge/Time';
-import './Challenge/style.css';
+// import Time from './Challenge/Time';
+// import './Challenge/style.css';
+
+import Practice from "./Destrucuring/Practice";
 
 function App() {
   return (
     <>
       {/* <UseState/> */}
-      <Time/>
+      {/* <Time/> */}
+      <Practice/>
     </>
   );
 }
