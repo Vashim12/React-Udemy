bfc9554b2e53924d79239802319a7c8ff0c9d1d1
Vashim Nadaf <vashimnadaf12@gmail.com>
2025-04-19 15:07:52 +0530

    Form

---
+++ event/src/App.js
@@ -0,0 +1,14 @@
+
+// import EventHandle from './EventHandle';
+import Form from "./Form/Form";
+
+function App() {
+  return (
+    <div>
+      {/* <EventHandle/> */}
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
